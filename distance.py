3a9783979f73fd60486fece8206809085f5e1769
matthewcuevas <matthewxcuevas@gmail.com>
2018-10-31 20:39:16 -0400

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,12 +1,12 @@
 from skimage import io
 import matplotlib.pyplot as plot
 from PIL import Image
+import numpy as np
 
 def get_edges(im, sigma):
     from skimage import img_as_float
     from skimage.feature import canny
     from skimage.color import rgb2gray
-    from autoCanny import auto_canny
 
     inputImage = img_as_float(im)
     inputGrayscaleImage = rgb2gray(inputImage)
@@ -14,24 +14,32 @@ def get_edges(im, sigma):
     return edges
 
 def get_distance(edges1, edges2):
-    from scipy.spatial.distance import directed_hausdorff
+    err = np.sum((edges1.astype("float") - edges2.astype("float")) ** 2) / float(edges1.shape[0] * edges2.shape[1])
 
-    return directed_hausdorff(edges1, edges2)
+    return err
 
-im1 = io.imread('whitepeas1.png')
-im2 = io.imread('whitepeas3.png')
+im1path = 'chickpeas1.png'
+im2path = 'whitepeas1.png'
+im1 = io.imread(im1path)
+im2 = io.imread(im2path)
+# im2 = np.random.random(im1.shape)
+sigma2 = 3
 
 plot.subplot(2, 2, 1)
+plot.title(im1path)
 plot.imshow(im1)
 
 plot.subplot(2, 2, 2)
+plot.title(im2path)
 plot.imshow(im2)
 
 plot.subplot(2, 2, 3)
+plot.title(im1path + " Edges")
 plot.imshow(get_edges(im1, 3), cmap = 'gray')
 
 plot.subplot(2, 2, 4)
-plot.imshow(get_edges(im2, 3), cmap = 'gray')
+plot.title(im2path + " Edges")
+plot.imshow(get_edges(im2, sigma2), cmap = 'gray')
 plot.show()
 
-print(get_distance(get_edges(im1, 3), get_edges(im2, 3)))
+print(get_distance(get_edges(im1, 3), get_edges(im2, sigma2)))
